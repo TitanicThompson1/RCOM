d8622fd17f1d7f688a71f949a7981016beedb6f7
Ricardo Amaral Nunes <ricardo.nunes.1892@gmail.com>
2020-09-29 12:24:26 +0100

    Initial commit

---
+++ T1/emissor.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <macros.h>
+int main(void) {
+
+    char message[100];
+
+    //Reads from stdin 
+    if(gets(message) == NULL)
+        return ERROR;
+
+    
+    
+}
